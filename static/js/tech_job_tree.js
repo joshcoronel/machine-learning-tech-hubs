46e20ad913cf6a16607cf34af9f90b5333d4acf5
Keana Isabel Mabilog <keanaisabelmabilog@Keanas-MacBook-Pro.local>
2020-12-09 14:12:48 -0500

    add new cities to tech job viz

--- static/js/tech_job_tree.js
+++ static/js/tech_job_tree.js
@@ -8,7 +8,16 @@
           ['Austin',    'Tech Hub Cities',             0],
           ['New York City',      'Tech Hub Cities',             0],
           ['San Francisco', 'Tech Hub Cities',             0],
-          ['Chicago', 'Tech Hub Cities',             0],
+          ['Dallas', 'Tech Hub Cities',             0],
+          ['Raleigh', 'Tech Hub Cities',             0],
+          ['San Jose', 'Tech Hub Cities',             0],
+          ['Charlotte', 'Tech Hub Cities',             0],
+          ['Seattle', 'Tech Hub Cities',             0],
+          ['Huntsville', 'Tech Hub Cities',             0],
+          ['Denver', 'Tech Hub Cities',             0],
+          ['Boston', 'Tech Hub Cities',             0],
+          ['Trenton', 'Tech Hub Cities',             0],
+          
           
           // ATLANTA
           ['Computer Systems Analysts',    'Atlanta',            16280],
@@ -45,26 +54,6 @@
           ['Operations Research Analysts.',    'Austin',            1190],
           ['Statisticians.',    'Austin',            280],
           ['Data Scientists and Mathematical Science Occupations, All Other.',    'Austin',            370],
-          
-          // CHICAGO
-          ['Computer Systems Analysts..',    'Chicago',            22410],
-          ['Information Security Analysts..',       'Chicago',            3030],
-          ['Computer and Information Research Scientists..',    'Chicago',            380],
-          ['Computer Network Support Specialists..',    'Chicago',            9120],
-          ['Computer User Support Specialists..',    'Chicago',            19250],
-          ['Computer Network Architects..',    'Chicago',            4250],
-          ['Network and Computer Systems Administrators..',    'Chicago',            9460],
-          ['Database Administrators and Architects..',    'Chicago',            3780],
-          ['Computer Programmers..',    'Chicago',            7720],
-          ['Software Developers and Software Quality Assurance Analysts and Testers..',    'Chicago',            46970],
-          ['Web Developers and Digital Interface Designers..',    'Chicago',            4830],
-          ['Computer Occupations, All Other..',    'Chicago',            11040],
-          ['Actuaries..',    'Chicago',            1740],
-          ['Mathematicians',    'Chicago',            60],
-          ['Operations Research Analysts..',    'Chicago',            3370],
-          ['Statisticians..',    'Austin',            1550],
-          ['Data Scientists and Mathematical Science Occupations, All Other..',    'Chicago',            1570],
-          
           
           // New York
           ['Computer Systems Analysts...',    'New York City',            44210],
@@ -104,6 +93,164 @@
           ['Statisticians .',    'San Francisco',            1770],
           ['Data Scientists and Mathematical Science Occupations, All Other .',    'San Francisco',            1410],
        
+          //"Dallas, TX"
+          ['Computer Systems Analysts .',    'Dallas',            19460],
+          ['Information Security Analysts .',       'Dallas',            4690],
+          ['Computer and Information Research Scientists .',    'Dallas',            740],
+          ['Computer Network Support Specialists .',    'Dallas',            5300],
+          ['Computer User Support Specialists .',    'Dallas',            23120],
+          ['Computer Network Architects .',    'Dallas',            5690],
+          ['Network and Computer Systems Administrators .',    'Dallas',            12610],
+          ['Database Administrators and Architects .',    'Dallas',            4820],
+        
+          ['Computer Programmers .',    'Dallas',            9240],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Dallas',            51370],
+          ['Web Developers and Digital Interface Designers .',    'Dallas',            3850],
+          ['Computer Occupations, All Other .',    'Dallas',            11490],
+          ['Actuaries .',    'Dallas',            390],
+          ['Operations Research Analysts .',    'Dallas',            4370],
+          ['Statisticians .',    'Dallas',            540],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Dallas',            790],
+   
+          //"Raleigh, NC"
+          ['Computer Systems Analysts .',    'Raleigh',            7070],
+          ['Information Security Analysts .',       'Raleigh',            1420],
+          ['Computer Network Support Specialists .',    'Raleigh',            2070],
+          ['Computer User Support Specialists .',    'Raleigh',            5370],
+          ['Computer Network Architects .',    'Raleigh',            1410],
+          ['Network and Computer Systems Administrators .',    'Raleigh',            2860],
+          ['Database Administrators and Architects .',    'Raleigh',            800],
+        
+          ['Computer Programmers .',    'Raleigh',            1230],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Raleigh',            14600],
+          ['Web Developers and Digital Interface Designers .',    'Raleigh',            1040],
+          ['Computer Occupations, All Other .',    'Raleigh',            1170],
+          ['Actuaries .',    'Raleigh',            40],
+          ['Operations Research Analysts .',    'Raleigh',            480],
+          ['Statisticians .',    'Raleigh',            490],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Raleigh',            460],
+
+          //"San Jose, CA"
+          ['Computer Systems Analysts .',    'San Jose',            12190],
+          ['Information Security Analysts .',       'San Jose',            1330],
+          ['Computer Network Support Specialists .',    'San Jose',            1660],
+          ['Computer User Support Specialists .',    'San Jose',            2190],
+          ['Computer Network Architects .',    'San Jose',            12850],
+          ['Network and Computer Systems Administrators .',    'San Jose',            3330],
+          ['Database Administrators and Architects .',    'San Jose',            1980],
+        
+          ['Computer Programmers .',    'San Jose',            3510],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'San Jose',            81950],
+          ['Web Developers and Digital Interface Designers .',    'San Jose',            2550],
+          ['Computer Occupations, All Other .',    'San Jose',            13180],
+  
+          ['Operations Research Analysts .',    'San Jose',            1040],
+          ['Statisticians .',    'San Jose',            560],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'San Jose',            710],
+
+          //"Charlotte, NC"
+          ['Computer Systems Analysts .',    'Charlotte',            11310],
+          ['Information Security Analysts .',       'Charlotte',            3070],
+          ['Computer Network Support Specialists .',    'Charlotte',            1920],
+          ['Computer User Support Specialists .',    'Charlotte',            6250],
+          ['Computer Network Architects .',    'Charlotte',            1790],
+          ['Network and Computer Systems Administrators .',    'Charlotte',            3890],
+          ['Database Administrators and Architects .',    'Charlotte',            1510],
+        
+          ['Computer Programmers .',    'Charlotte',            1590],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Charlotte',            13370],
+          ['Web Developers and Digital Interface Designers .',    'Charlotte',            1440],
+          ['Computer Occupations, All Other .',    'Charlotte',            1770],
+          ['Operations Research Analysts .',    'Charlotte',            1770],
+          ['Statisticians .',    'Charlotte',            170],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Charlotte',            290],
+
+          //"Seattle, WA"
+          ['Computer Systems Analysts .',    'Seattle',            12590],
+          ['Information Security Analysts .',       'Seattle',            2480],
+          ['Computer Network Support Specialists .',    'Seattle',            1750],
+          ['Computer User Support Specialists .',    'Seattle',            2610],
+          ['Computer Network Architects .',    'Seattle',            13130],
+          ['Network and Computer Systems Administrators .',    'Seattle',            3030],
+          ['Database Administrators and Architects .',    'Seattle',            5940],
+        
+          ['Computer Programmers .',    'Seattle',            2230],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Seattle',            71940],
+          ['Web Developers and Digital Interface Designers .',    'Seattle',            12510],
+          ['Actuaries .',    'Seattle',            380],
+          ['Operations Research Analysts .',    'Seattle',            2180],
+          ['Statisticians .',    'Seattle',            1080],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Seattle',            1050],
+
+          //"Huntsville, Al"
+          ['Computer Systems Analysts .',    'Huntsville',            1600],
+          ['Information Security Analysts .',       'Huntsville',            710],
+          ['Computer Network Support Specialists .',    'Huntsville',            410],
+          ['Computer User Support Specialists .',    'Huntsville',            290],
+          ['Computer Network Architects .',    'Huntsville',            1120],
+          ['Network and Computer Systems Administrators .',    'Huntsville',            530],
+          ['Database Administrators and Architects .',    'Huntsville',            780],
+        
+          ['Computer Programmers .',    'Huntsville',            390],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Huntsville',            2400],
+          ['Web Developers and Digital Interface Designers .',    'Huntsville',            6610],
+          ['Computer Occupations, All Other .',    'Huntsville',            210],
+          ['Operations Research Analysts .',    'Huntsville',            460],
+
+          //"Denver, CO"
+          ['Computer Systems Analysts .',    'Denver',            6710],
+          ['Information Security Analysts .',       'Denver',            2440],
+          ['Computer Network Support Specialists .',    'Denver',            3130],
+          ['Computer User Support Specialists .',    'Denver',            9250],
+          ['Computer Network Architects .',    'Denver',            5040],
+          ['Network and Computer Systems Administrators .',    'Denver',            8320],
+          ['Database Administrators and Architects .',    'Denver',            1230],
+        
+          ['Computer Programmers .',    'Denver',            1740],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Denver',            28740],
+          ['Web Developers and Digital Interface Designers .',    'San Jose',            2070],
+          ['Computer Occupations, All Other .',    'Denver',            9260],
+          ['Actuaries .',    'Denver',            210],
+          ['Operations Research Analysts .',    'Denver',            490],
+          ['Statisticians .',    'Denver',            810],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Denver',            490],
+
+         
+          //"Boston, MA"
+          ['Computer Systems Analysts .',    'Boston',            15430],
+          ['Information Security Analysts .',       'Boston',            4010],
+          ['Computer Network Support Specialists .',    'Boston',            4300],
+          ['Computer User Support Specialists .',    'Boston',            17040],
+          ['Computer Network Architects .',    'Boston',            5440],
+          ['Network and Computer Systems Administrators .',    'Boston',            7710],
+          ['Database Administrators and Architects .',    'Boston',            3120],
+        
+          ['Computer Programmers .',    'Boston',            4150],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'Boston',            55260],
+          ['Web Developers and Digital Interface Designers .',    'Boston',            4270],
+          ['Actuaries .',    'Boston',            960],
+          ['Operations Research Analysts .',    'Boston',            2330],
+          ['Statisticians .',    'Boston',            2670],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'Boston',            520],
+
+          //"Trenton, NJ"
+          ['Computer Systems Analysts .',    'San Jose',            1390],
+          ['Information Security Analysts .',       'San Jose',            210],
+          ['Computer Network Support Specialists .',    'San Jose',            40],
+          ['Computer User Support Specialists .',    'San Jose',            1260],
+          ['Computer Network Architects .',    'San Jose',            270],
+          ['Network and Computer Systems Administrators .',    'San Jose',            1000],
+          ['Database Administrators and Architects .',    'San Jose',            360],
+        
+          ['Computer Programmers .',    'San Jose',            660],
+          ['Software Developers and Software Quality Assurance Analysts and Testers .',    'San Jose',            4140],
+          ['Web Developers and Digital Interface Designers .',    'San Jose',            270],
+          ['Computer Occupations, All Other .',    'San Jose',            530],
+          ['Actuaries .',    'San Jose',            50],
+          ['Operations Research Analysts .',    'San Jose',            200],
+          ['Statisticians .',    'San Jose',            300],
+          ['Data Scientists and Mathematical Science Occupations, All Other .',    'San Jose',            120],
+
           
  
         ]);
